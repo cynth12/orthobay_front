48876e85368552403e71b066eb6f3fa77088ae52
cynth12 <gonzalez.cynth12@gmail.com>
2025-04-14 18:18:12 -0500

    home

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 //Route::get('/', function () {
-    //return view('welcome');
+    //return view('');
 //});
 
-Route::get('/{lang}/home', [HomeController::class, 'index'])
-    ->where('lang', 'es|en');
+//Route::get('/{lang}/home', [HomeController::class, 'index'])
+   // ->where('lang', 'es|en');
+
+Route::redirect('/', '/en');
+
+
+Route::group(['prefix' => 'en'], function () {
+    Route::get('/', [HomeController::class, 'index'])->name('en.home');
+
+});
+
+
+
+
+Route::group(['prefix' => 'es'], function () {
+    Route::get('/', [HomeController::class, 'index'])->name('es.inicio');
+
+});
+    
