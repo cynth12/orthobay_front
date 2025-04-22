e4743261f12f1db5ee028c0ed272a5b9885049e6
cynth12 <gonzalez.cynth12@gmail.com>
2025-04-21 20:09:42 -0500

    rutas

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 
-Route::get('/test', function () {
-    return 'La web sí responde 🧡';
-});
-
-
 
 
 Route::get('/', function () {
