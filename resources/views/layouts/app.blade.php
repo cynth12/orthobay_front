2e0e35fc8b412b979d4c0e23322eeccc18f01a29
cynth12 <gonzalez.cynth12@gmail.com>
2025-04-22 13:53:22 -0500

    logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 	<title>Smartdev Medical Responsive Website Template</title>
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" type="image/png" href="{{ asset('assets/images/favicon.png')}}">
+	<link rel="icon" type="image/png" href="{{ asset('assets/images/favicon_otrhobay_huesito.png')}}">
 	@include('layouts.commonCSS')
 </head>
 
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -46,7 +46,7 @@
                                                     <img src="{{ asset('assets/images/logo_1.png') }}" alt="smartdev">
                                                 </a>
                                                 <a class="main_sticky_l" href="index.html" title="smartdev">
-                                                    <img src="{{ asset('assets/images/logo_blanco.png') }}" alt="smartdev">
+                                                    <img src="{{ asset('assets/images/logo_2.png') }}" alt="smartdev">
                                                 </a>
                                             </div>
                                         </div>
@@ -105,9 +105,6 @@
 <!-- MOBILE MENU AREA -->
 <div class="home-2 mbm hidden-md hidden-lg  header_area main-menu-area">
 	<div class="menu_area mobile-menu">
-        <button id="menu-toggle" class="menu-toggle-btn">
-            ☰
-          </button>
 		<nav class="smartdev_menu">						
 			<ul class="sub-menu">
             @if (app()->getLocale() == 'es')
